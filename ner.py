26344764d33836a28bb7214d4bfddc9f1f8647ac
Joe Antaki <antakij1@students.rowan.edu>
2019-12-17 13:51:11 -0500

    appends all titles to named entities

--- ner.py
+++ ner.py
@@ -1,7 +1,6 @@
 import spacy
 import preprocessing
 from nltk.probability import FreqDist
-from nltk.tokenize import word_tokenize
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -15,18 +14,18 @@ death_words = preprocessing.murder_words(text)
 murdery_sents = preprocessing.murder_sents(sent_words, sentences, death_words)
 
 ents = []
+titles = ['Master ', 'Mr. ', 'Miss ', 'Ms. ', 'Mrs. ', 'Sir ', 'Madam ', 'Dame ', 'Lord ', 'Lady ', 'Doctor ', 'Dr. ', 'Professor ', 'Fr. ', 'Pr. ', 'Br. ', 'Sr. ', 'Father ', 'Pastor ', 'Brother ', "Sister "]
 
 for tup in murdery_sents:
-    for i in range(1,6):
+    for i in range(1,len(tup)):
         doc = nlp(tup[i])
         for ent in doc.ents:
             if ent.label_ == "PERSON":
                 to_append = ent.text
-                if ent.start_char > 3 and tup[i][(ent.start_char-4):(ent.start_char)] == "Mr. ":
-                    to_append = "Mr. " + to_append
-                elif ent.start_char > 4 and tup[i][(ent.start_char-5):(ent.start_char)] == "Mrs. ":
-                    to_append = "Mrs. " + to_append
-
+                for t in titles:
+                    if ent.start_char >= len(t) and tup[i][(ent.start_char-len(t)):(ent.start_char)] == t:
+                        to_append = t + to_append
+                        break
                 ents.append(to_append)
 
 counts = FreqDist(ents)
