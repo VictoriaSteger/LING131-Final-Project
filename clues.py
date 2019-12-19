dbea45171fa6defcd5736c6a3bbf76b4b4b8ee35
VICTORIA_STEGER <victoriasteger@brandeis.edu>
2019-12-19 13:43:27 -0500

    Adjusted main to loop through files for testing, adjusted preprocessing to use 'fatal' as the target adjective, added another loop in clues (change made by Bailey, committed by me) to account for murdery_sents being a 6 part tuple

--- clues.py
+++ clues.py
@@ -4,26 +4,28 @@ I uploaded it as ner2
 import ner2
 from nltk.tokenize import RegexpTokenizer
 
+
 def clues(murdery_sents):
-    
+
     people = list(set(ner2.listofPeople(murdery_sents)))
-    
+
     new_people = []
-    
+
     for i in people:
         if str(i) not in new_people:
             new_people.append(str(i))
-    
+
     new_murder = []
-    
+
     tokenizer = RegexpTokenizer(r'\w+')
-    
+
     for i in new_people:
         for j in murdery_sents:
-            k = tokenizer.tokenize(j[1])
-            if i in k:
-                new_murder.append(j)
-            elif "Mr." in k or "Mrs." in k:
-                new_murder.append(j)
+            for l in j:
+                k = tokenizer.tokenize(l)
+                if i in l:
+                    new_murder.append(j)
+                elif "Mr." in k or "Mrs." in k:
+                    new_murder.append(j)
 
     return list(new_murder)
