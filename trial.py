a6bc6d24fd743bd283156fa1d21d85a460a63140
Joe Antaki <antakij1@students.rowan.edu>
2019-12-19 14:22:02 -0500

    working trial

--- trial.py
+++ trial.py
@@ -5,31 +5,42 @@ import them_roles
 import ner2
 
 def testing():
+    tested = 0
+    kill_acc = 0
+    vic_acc = 0
+
     with open("answers-story,killer,victim.txt") as file:
         for line in file:
+            tested += 1
+
             f, s, t = line.split(",")
+            # To discard newline character
+            t = t[:-1]
 
-            print("doing", f)
+            with open("data/" + f + ".txt") as story:
+                text = story.read()
 
-            name = f + ".txt"
+                sent_words, sentences = preprocessing.lemma_sentences(text)
 
-            sent_words, sentences = preprocessing.lemma_sentences(name)
-
-            # get a list of words from the text related to death/murder
-            death_words = preprocessing.improved_murder_words(name)
+                # get a list of words from the text related to death/murder
+                death_words = preprocessing.improved_murder_words(text)
 
             # get a list of sentences and context for death/murder words
             murdery_sents = preprocessing.murder_sents(sent_words, sentences, death_words)
 
-            # not currently used by Mike's method
+            # filter murdery_sents to only keep entries with a character name in one of the sentences
             cl = clues.clues(murdery_sents)
+
+            # get a list of people in the story
             people = ner2.listofPeople(murdery_sents)
 
+            # change the list from objects to strings
             new_people = []
             for i in people:
                 if str(i) not in new_people:
                     new_people.append(str(i).lower())
 
+            # attempt to predict the murderer and victim
             kill, vic = them_roles.murder_aggregate(cl, death_words, new_people)
 
             print("Story:", f)
@@ -38,26 +49,14 @@ def testing():
             print("\tReal victim:", t)
             print("\tPredicted victim:", vic)
 
-def bad_detective(people):
-    rand = random.randint(0,(len(people)))
-    definitely_the_killer = people[rand]
-    return definitely_the_killer
+            if s in kill or kill in s:
+                kill_acc += 1
+            if t in vic or vic in t:
+                vic_acc += 1
 
-def red_herring_sucess_rate(killer, people):
-    truth = 0
-    for i in range(100):
-        definitely_the_killer = bad_detective(people)
-        if definitely_the_killer == killer:
-            truth += 1
-    result = truth/100
-    return result
-
-def actual_success_rate(outcomes, num_tested):
-    return outcomes/num_tested
-
-
-def formula():
-    pass
+    print()
+    print("Killer prediction accuracy:", kill_acc/tested)
+    print("Victim prediction accuracy:", vic_acc / tested)
 
 if __name__ == "__main__":
     testing()
